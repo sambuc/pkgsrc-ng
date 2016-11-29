03b9ac22544095682de68dfa2f16a606e98623dd
Lionel Sambuc <lionel.sambuc@gmail.com>
2016-11-29 23:20:11 +0100

    Revert "Step 2: base tools: lang/clang"
    This reverts commit f5c35536fcd8604dc467e186c285aa5ab12631c7.

--- lang/clang/buildlink3.mk
+++ lang/clang/buildlink3.mk
@@ -1,13 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.1 2010/02/13 16:13:52 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2016/03/10 15:01:52 tnn Exp $
 
 BUILDLINK_TREE+=	clang
 
 .if !defined(CLANG_BUILDLINK3_MK)
 CLANG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.clang+=	clang>=2.6nb2
+BUILDLINK_API_DEPENDS.clang+=	clang>=3.8.0
 BUILDLINK_PKGSRCDIR.clang?=	../../lang/clang
 
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../lang/llvm/buildlink3.mk"
 .endif	# CLANG_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-clang
